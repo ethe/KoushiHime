6140720d632b7d413bfba9d718326404f64f9bb3
kafuuchino <soranomethods@yahoo.co.jp>
2016-02-04 22:06:15 +0900

    make folder tidier

--- app/main/form.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-from wtforms import StringField
-class PushForm(Form):
-    title=StringField('词条名',[validators.Required(),validators.Length(min=1,max=256)])
-    submit=SubmitField('推送')
--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask import render_template,session,redirect,url_for,request,abort
 from flask.ext.login import login_required
 from .. model import Page
+from datetime import datetime
 import pdb
 from . import main
 @main.route('/')
@@ -13,6 +14,7 @@ def update():
     p=Page()
     title=[]
     title=p.GetTitles()
+    pdb.settrace()
     if request.method == 'POST':
         jsondata=request.get_json()
         print jsondata
@@ -22,5 +24,4 @@ def update():
             pass
         else: 
             abort(403)
-    return render_template('update.html',title=title)
-
+    return render_template('update.html',title=title,current_time=datetime.utcnow())
