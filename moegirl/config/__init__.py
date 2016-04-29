3762f067025ce497f5bc0536ed82a72ab6be9db2
Zeno Go <lambda.guo@gmail.com>
2016-04-29 18:25:08 +0800

    Fix bugs when init database

--- moegirl/config/__init__.py
+++ moegirl/config/__init__.py
@@ -21,19 +21,19 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-                   'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, '../data-dev.sqlite')
 
 
 class TestingConfig(Config):
     TESTING = True
-    DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-                   'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, '../data-test.sqlite')
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+        'sqlite:///' + os.path.join(basedir, '../data.sqlite')
 
 
 config = {
