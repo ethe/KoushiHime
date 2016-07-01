393f813867dad176e0d5793f82a606c767145194
zeno <gotzehsing@gmail.com>
2016-07-01 19:08:47 +0800

    Change dumplicate entry catching settings.

--- koushihime/main/utils.py
+++ koushihime/main/utils.py
@@ -4,7 +4,7 @@ import datetime
 from koushihime.main.models import PushRecord, WaitingQueue
 
 
-def recent_have_pushed(title, hours=24):
+def recent_have_pushed(title, hours=72):
     limit_date = datetime.datetime.utcnow() - datetime.timedelta(hours=hours)
     query = PushRecord.query.filter(PushRecord.title == title, PushRecord.pushed_time > limit_date).all()
     if query:
