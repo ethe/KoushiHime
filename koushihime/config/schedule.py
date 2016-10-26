d40811819c02f5bf83007d8b303fea92e362269a
Gwo <lambda.guo@gmail.com>
2016-10-26 11:33:16 +0800

    Add celery task expire time settings.

--- koushihime/config/schedule.py
+++ koushihime/config/schedule.py
@@ -10,26 +10,41 @@ class CelerySchedule:
             'task': 'tasks.push',
             'schedule': crontab(hour='0-1', minute='0,30'),
             'args': (),
+            'options': {
+                    'expires': 5*60,
+                    },
         },
         'early_morning_push': {
             'task': 'tasks.push',
             'schedule': crontab(hour='2-5', minute='0'),
             'args': (),
+            'options': {
+                    'expires': 5*60,
+                    },
         },
         'morning_push': {
             'task': 'tasks.push',
             'schedule': crontab(hour='6-9', minute='0,30'),
             'args': (),
+            'options': {
+                    'expires': 5*60,
+                    },
         },
         'afternoon_push': {
             'task': 'tasks.push',
             'schedule': crontab(hour='10-17', minute='0,0-59/15'),
             'args': (),
+            'options': {
+                    'expires': 5*60,
+                    },
         },
         'late_afternoon_push': {
             'task': 'tasks.push',
             'schedule': crontab(hour='18-21', minute='0,0-59/10'),
             'args': (),
+            'options': {
+                    'expires': 5*60,
+                    },
         },
         'evening_push': {
             'task': 'tasks.push',
@@ -40,10 +55,16 @@ class CelerySchedule:
             'task': 'tasks.check_update',
             'schedule': crontab(hour='0-23', minute='0,0-59/15'),
             'args': (),
+            'options': {
+                    'expires': 5*60,
+                    },
         },
         'reset': {
             'task': 'tasks.reset',
             'schedule': crontab(hour='0', minute='5'),
             'args': (),
+            'options': {
+                    'expires': 5*60,
+                    },
         }
     }
